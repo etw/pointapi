8e3cf2acc0e29d182de72ebde0db8d85ef8ad271
Etw <etw@users.noreply.github.com>
2015-06-01 00:08:32 +0300

    Introduce types

--- posts.go
+++ posts.go
@@ -5,12 +5,12 @@ import (
 	"strings"
 )
 
-func (api *PointAPI) GetAll(before int) (map[string]interface{}, error) {
+func (api *PointAPI) GetAll(before int) (PostList, error) {
 	var path = fmt.Sprintf("/all?before=%i", before)
 	return api.metaGet(path)
 }
 
-func (api *PointAPI) GetTags(before int, tags []string) (map[string]interface{}, error) {
-	var path = fmt.Sprintf("/tags?before=%i&tag=%s", before, strings.Join(tags, "&tag="))
+func (api *PointAPI) GetTags(before int, tags []string) (PostList, error) {
+	var path = fmt.Sprintf("/tags?before=%s&tag=%s", before, strings.Join(tags, "&tag="))
 	return api.metaGet(path)
 }
